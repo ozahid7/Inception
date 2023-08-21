b7a31fea68e18fd973a60a58c59a98346f294080
ozahid7 <zhdosm@gmail.com>
2023-08-22 00:19:35 +0100

    Chore: create required files

--- srcs/requirements/mariadb/script.sh
+++ srcs/requirements/mariadb/script.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 service mysql start
+sleep 2
 sed -i 's@= 127.0.0.1@= 0.0.0.0@' /etc/mysql/mariadb.conf.d/50-server.cnf
 mysql -u root -e "create database wordpress;"
 mysql -u root -e "create user 'ozahid'@'%' identified by '1234';"
 mysql -u root -e "grant all privileges on wordpress.* to 'ozahid'@'%';"
 mysql -u root -e "alter user 'root'@'localhost' identified by '1234';"
-
 
 kill $(cat /var/run/mysqld/mysqld.pid)
 
