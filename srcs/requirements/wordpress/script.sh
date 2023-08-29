9bda250d3978114f27fb3a128321020f12474301
Oussama  Zahid <ozahid-@e3r7p14.1337.ma>
2023-08-29 02:27:33 +0100

    Feat: wordpress is done

--- srcs/requirements/wordpress/script.sh
+++ srcs/requirements/wordpress/script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sed -i 's@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
-service php7.4-fpm start
+# service php7.4-fpm start
+mkdir -p /run/php
 wp core download --allow-root
 wp core config --dbhost=mariadb --dbname=wordpress --dbuser=ozahid --dbpass=1234 --allow-root
-wp core install --url=ozahid-.42.fr --title="ozahid title" --admin_user=Admin --admin_password=1234 --admin_email=test@email.com --allow-root 
+wp core install --url=localhost --title="ozahid title" --admin_user=Admin --admin_password=1234 --admin_email=test@email.com --allow-root 
 
-
-sleep 1000000
+php-fpm7.4 -F
