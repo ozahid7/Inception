0a09d8cc31cdeaa76694e1e762a88a7c09e81727
ossama zahid <ozahid-@debian.ozahid-.42.fr>
2023-09-01 01:23:07 -0500

    Feat: add author user wordpress

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -6,6 +6,7 @@ config_file=/var/www/html/wp-config.php
 if [ ! -f "$config_file" ]; then
 	wp core download --allow-root
 	wp core config --dbhost=$WORDPRESS_DB_HOST --dbname=$MARIADB_DB --dbuser=$MARIADB_USER --dbpass=$MARIADB_PASS --allow-root
+	wp user create $WORDPRESS_AUTHOR_USER zoussama@test.com --user_pass=$MARIADB_PASS --role=author --allow-root
 	wp core install --url=$WORDPRESS_URL --title="Title" --admin_user=$MARIADB_USER --admin_password=$MARIADB_PASS --admin_email=test@email.com --allow-root
 fi
 php-fpm7.4 -F
